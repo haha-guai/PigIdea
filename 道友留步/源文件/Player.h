55fa508d51d3d87e30f46ad8e2021a366d8fdb42
岁岁与 <lyfx1314520@qq.com>
2025-06-30 22:20:50 +0800

    创建新项目,搞了个随随便便的头文件

---
+++ 道友留步/源文件/Player.h
@@ -0,0 +1,18 @@
+#pragma once
+#include<string>
+#include<vector>
+
+class Player
+{
+private:
+	//昵称
+	//属性列表
+	//功法列表
+	//背包
+	//杂七杂八
+	//称号列表
+	std::string name;
+	std::vector<std::string> titleList;
+
+};
+
